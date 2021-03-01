28f3c0cc2a8778f34e9225862700d3e4eb98051f
ByJIM <jiguilmor@gmail.com>
2021-03-01 20:50:17 +0100

    Colores 01LUN2049

--- app/src/main/java/com/example/myapplication/ConfigFragment.java
+++ app/src/main/java/com/example/myapplication/ConfigFragment.java
@@ -1,12 +1,24 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,14 +40,8 @@ public class ConfigFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfigFragment.
-     */
+   Button btnColor1;
+
     // TODO: Rename and change types and number of parameters
     public static ConfigFragment newInstance(String param1, String param2) {
         ConfigFragment fragment = new ConfigFragment();
@@ -53,12 +59,73 @@ public class ConfigFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        
     }
 
+
+    private int color;
+    public void onClic(View view){
+        switch(view.getId()){
+            case R.id.btnColor1:
+                DameColor(color);
+                break;
+        }
+    }
+
+    private void DameColor(int currentColor){
+        Log.d("PRIMER COLOR", color+"");
+        ColorPickerDialogBuilder
+                .with(getContext())
+                .setTitle("Choose color")
+                .initialColor(currentColor)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        //toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        color = selectedColor;
+                        Log.d("CAMBIO COLOR", color+"");
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        
+                    }
+                })
+                .build()
+                .show();
+
+
+
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_config, container, false);
+        View view = inflater.inflate(R.layout.fragment_config, container, false);
+        btnColor1 = view.findViewById(R.id.btnColor1);
+
+        color = ((ColorDrawable)btnColor1.getBackground()).getColor();
+        btnColor1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClic(v);
+            }
+        });
+
+        return view;
+
+
     }
+
+
 }
